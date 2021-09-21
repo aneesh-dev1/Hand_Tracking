31110aee08f7b9ca5c53b7820861fa1dc44e1732
Aneesh Kumar <71940535+aneesh-learner@users.noreply.github.com>
2021-09-21 22:52:56 +0530

    Add files via upload

---
+++ HandTracking/HandTracking.py
@@ -0,0 +1,36 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+mpHand = mp.solutions.hands
+hands = mpHand.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result= hands.process(imgRGB)
+    #print(result.multi_hand_landmarks)
+    if result.multi_hand_landmarks:
+        for handLms in result.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if(id==4):
+                    cv2.circle(img, (cx, cy),15, (255,0,255), cv2.FILLED)
+            
+            
+            
+            mpDraw.draw_landmarks(img, handLms, mpHand.HAND_CONNECTIONS)
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_ITALIC,1,(255, 0, 255),3)
+    cv2.imshow("Image",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+     break
